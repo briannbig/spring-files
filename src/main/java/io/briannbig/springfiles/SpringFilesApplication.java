b383eb180cf8d4cb24870d04643be038c726f208
Brian Barasa <briannbig@gmail.com>
2023-07-19 10:08:10 +0300

    add controller

--- src/main/java/io/briannbig/springfiles/SpringFilesApplication.java
+++ src/main/java/io/briannbig/springfiles/SpringFilesApplication.java
@@ -1,13 +1,27 @@
 package io.briannbig.springfiles;
 
+import io.briannbig.springfiles.storage.StorageProperties;
+import io.briannbig.springfiles.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class SpringFilesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringFilesApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
